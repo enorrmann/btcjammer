f759f8ea617ef8763dbd72238ff2dbce574eb49f
enorrmann <enorrmann@gmail.com>
2014-04-22 12:48:49 -0300

    formatting

--- plugin/lib/main.js
+++ plugin/lib/main.js
@@ -3,29 +3,30 @@ var self = require("sdk/self");
 var Request = require("sdk/request").Request;
 
 require("sdk/tabs").on("ready", runScript);
- 
+
 function runScript(tab) {
-var taburl = tab.url+"";
-if (taburl.indexOf("btcjam.com/users") == -1){
-	return;
-}
 
-var lasti = taburl.lastIndexOf("/");
-var userId  = taburl.substr(lasti+1);
+	var taburl = tab.url + "";
+	
+	if (taburl.indexOf("btcjam.com/users") == -1) {
+		return;
+	}
 
-  var w1 = tab.attach({
-    contentScriptFile: [self.data.url("jquery-1.4.2.min.js"), self.data.url("jammer.js")]
-  });
+	var lasti = taburl.lastIndexOf("/");
+	var userId = taburl.substr(lasti + 1);
 
-	var jammerRequest = Request({
-	  url: "http://jam.sd.ai/users/"+userId+"/al_chart_data",
-	  onComplete: function (response) {
-w1.port.emit("replacePage", response.text);
-	  }
+	var w1 = tab.attach({
+		contentScriptFile : [ self.data.url("jquery-1.4.2.min.js"),
+				self.data.url("jammer.js") ]
 	});
 
-jammerRequest.get();
+	var jammerRequest = Request({
+		url : "http://jam.sd.ai/users/" + userId + "/al_chart_data",
+		onComplete : function(response) {
+			w1.port.emit("replacePage", response.text);
+		}
+	});
 
-
+	jammerRequest.get();
 
 }
